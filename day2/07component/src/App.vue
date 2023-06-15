d1792b8665ed793d48e57f092a11dc1e65d3f4dc
1263335155@qq.com <1263335155@qq.com>
2023-06-15 22:04:40 +0800

    day2

---
+++ day2/07component/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    data() {
+        return {
+            message: "我爱vue",
+        };
+    },
+}
+</script>
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
