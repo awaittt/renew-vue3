43ab7251fc0defb89c5c34660b07b6db17c5b8eb
1263335155@qq.com <1263335155@qq.com>
2023-06-29 22:00:10 +0800

    17

--- 09auto_created/src/App.vue
+++ 09auto_created/src/App.vue
@@ -1,13 +1,24 @@
 <template >
     
-    <div>{{msg}}</div>
+    <div>
+        <div>{{msg}}</div>
+        <div>{{ count(1,2) }}</div>
+        <button @click="change">点我更新消息</button>
+    </div>
 
-    
 </template>
 <script>
 export default {
             data(){
                 return {msg:'哈哈哈哈哈'}
+            },
+            methods:{
+                count(a,b){
+                    return a+b
+                },
+                change(){
+                    this.msg='消息更新了'
+                }
             }
         }
 </script>
